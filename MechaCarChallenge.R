033762242c637d4801286d89c6c4b5240b17402a
Tony Ferri <antonio.m.ferri@gmail.com>
2021-12-27 10:15:03 -0500

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,10 +1,29 @@
-library(dplyr)
+library(tidyverse)
 mecha_table <- read.csv("MechaCar_mpg.csv")
 head(mecha_table)
-library(ggplot2)
 
-plt <- ggplot(mecha_table,aes(x=vehicle_weight,y=mpg))
-plt + geom_point()
+# plt <- ggplot(mecha_table,aes(x=vehicle_weight,y=mpg))
+# plt + geom_point()
+# 
+# mecha_matrix <- as.matrix(mecha_table[,c("vehicle_weight","spoiler_angle","ground_clearance","AWD","vehicle_length","mpg")])
+# cor(mecha_matrix)
 
 lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length,data=mecha_table)
 summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length,data=mecha_table))
+
+suspension_table <- read.csv("Suspension_Coil.csv")
+head(suspension_table)
+
+# lm(Manufacturing_Lot ~ VehicleID + PSI,data=suspension_table)
+# summary(lm(Manufacturing_Lot ~ VehicleID + PSI,data=suspension_table))
+
+# ?summarize()
+
+# total_summary <- suspension_table %>% summarize(Mean=mean(),Median=median(),Variance=var(),SD=sd(), .groups = "drop")
+# rlang::last_error()
+
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MechaCar_Statistical_Analysis
+
+## Linear Regression to Predict MPG
+* Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
+  * Vehicle Length (2.60e-12), Ground Clearance (5.21e-08) and the intercept (5.08e-08)
+* Is the slope of the linear model considered to be zero? Why or why not?
+  * Multiple R-squared value is 0.7149
+  * p-value is 5.35e-11
+  * Since the p-value is much smaller than the significance level of 0.05%, we can state that the slope is not zero (and also reject the null hypothesis). 
+* Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
+  * Since only two of the six variables are considered significant, this would not effectively predict mpg
+
