ae653ef71f7bf5668f0c64f2fc7ca82b24513204
Tony Ferri <antonio.m.ferri@gmail.com>
2021-12-28 12:52:37 -0500

    added p value pics

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,29 +1,29 @@
 library(tidyverse)
 mecha_table <- read.csv("MechaCar_mpg.csv")
-head(mecha_table)
-
-# plt <- ggplot(mecha_table,aes(x=vehicle_weight,y=mpg))
-# plt + geom_point()
-# 
-# mecha_matrix <- as.matrix(mecha_table[,c("vehicle_weight","spoiler_angle","ground_clearance","AWD","vehicle_length","mpg")])
-# cor(mecha_matrix)
+# head(mecha_table)
 
 lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length,data=mecha_table)
 summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length,data=mecha_table))
 
 suspension_table <- read.csv("Suspension_Coil.csv")
-head(suspension_table)
-
-# lm(Manufacturing_Lot ~ VehicleID + PSI,data=suspension_table)
-# summary(lm(Manufacturing_Lot ~ VehicleID + PSI,data=suspension_table))
-
-# ?summarize()
-
-# total_summary <- suspension_table %>% summarize(Mean=mean(),Median=median(),Variance=var(),SD=sd(), .groups = "drop")
-# rlang::last_error()
+# head(suspension_table)
 
 total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
+population_table <- suspension_table %>% sample_n(50)
 
+population_lot1 <- subset(population_table, Manufacturing_Lot == "Lot1")
+population_lot2 <- subset(population_table, Manufacturing_Lot == "Lot2")
+population_lot3 <- subset(population_table, Manufacturing_Lot == "Lot3")
+
+suspension_lot1 <- subset(suspension_table, Manufacturing_Lot == "Lot1")
+suspension_lot2 <- subset(suspension_table, Manufacturing_Lot == "Lot2")
+suspension_lot3 <- subset(suspension_table, Manufacturing_Lot == "Lot3")
+
+t.test(log10(suspension_table$PSI),mu=mean(log10(population_table$PSI))) #compare sample versus population means (total)
+
+t.test(log10(suspension_lot1$PSI),mu=mean(log10(population_lot1$PSI))) #compare sample versus population means (lot 1)
+t.test(log10(suspension_lot2$PSI),mu=mean(log10(population_lot2$PSI))) #compare sample versus population means (lot 2)
+t.test(log10(suspension_lot3$PSI),mu=mean(log10(population_lot3$PSI))) #compare sample versus population means (lot 3)
--- README.md
+++ README.md
@@ -10,3 +10,17 @@
 * Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
   * Since only two of the six variables are considered significant, this would not effectively predict mpg
 
+[MPG Linear Regression](https://github.com/tonyferri/MechaCar_Statistical_Analysis/blob/main/Resources/mpg%20linear%20regression%20del%201.png);
+
+## Summary Statistics on Suspension Coils
+* The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch.
+  * If measured only in total, this criteria is met due to the overall variance being equal to 62.29 PSI.
+  * If measured individually, Lot 3 does not meet the criteria with a PSI variance of 170.29. 
+
+[Total Summary](https://github.com/tonyferri/MechaCar_Statistical_Analysis/blob/main/Resources/total%20summary%20del%202.png);
+
+[Lot Summary](https://github.com/tonyferri/MechaCar_Statistical_Analysis/blob/main/Resources/lot%20summary%20del%202.png);
+
+## T-Tests on Suspension Coils
+
+## Study Design: MechaCar vs Competition
